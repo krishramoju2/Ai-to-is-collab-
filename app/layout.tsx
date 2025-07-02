e92af1f210d2b8dff595cbdc0b8b7b06426a9243
krishramoju2 <futureai.incorporated@gmail.com>
2025-07-02 15:15:34 +0530

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import './globals.css';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-900 text-white font-sans">{children}</body>
-    </html>
-  );
-}
